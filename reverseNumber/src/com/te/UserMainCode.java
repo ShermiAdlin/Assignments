a13a385dfe4a2979931556e0b693f4523e0495a7
Shermi Adlin <shermivijay@gmail.com>
2022-08-18 17:20:41 +0530

    first commit

---
+++ reverseNumber/src/com/te/UserMainCode.java
@@ -0,0 +1,13 @@
+package com.te;
+
+public class UserMainCode {
+	public static int reverseNumber(int n) {
+		int rev = 0;
+		while (n > 0) {
+			rev = rev * 10 + (n % 10);
+			n = n / 10;
+		}
+		return rev;
+
+	}
+}
